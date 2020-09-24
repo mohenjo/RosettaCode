82fb9833cb603ed219f2087633afaee9c4da6990
mohenjo <66149782+mohenjo@users.noreply.github.com>
2020-09-24 18:31:05 +0900

    Update main.py

--- src/Biorhythms/main.py
+++ src/Biorhythms/main.py
@@ -1,6 +1,5 @@
 import datetime
 import math
-import matplotlib.pyplot as plt
 
 
 def calc_biorhythms(birthday: datetime.datetime, targetday: datetime.datetime):
@@ -25,31 +24,59 @@ def calc_biorhythms(birthday: datetime.datetime, targetday: datetime.datetime):
     return day_elapsed, (physical, emotional, mental)
 
 
-def get_biorhythms(birthday: str, targetday: str):
+def main():
+    # inputstr = input("input birthday and targetday (eg. 1991-01-01 1999-12-31): ")
+    # birthday, targetday = inputstr.split()
+    birthday = "1943-03-09"
+    targetday = "1972-07-11"
     birthday_dt = datetime.datetime.strptime(birthday, "%Y-%m-%d")
     targetday_dt = datetime.datetime.strptime(targetday, "%Y-%m-%d")
 
+    ### CL
+    rst = calc_biorhythms(birthday_dt, targetday_dt)
 
-def main():
-    birthday_dt = datetime.datetime.strptime("1943-03-09", "%Y-%m-%d")
-    targetday_dt = datetime.datetime.strptime("1972-07-11", "%Y-%m-%d")
-    elapsed_td = targetday_dt - birthday_dt
-    x_series = []
-    y_series = []
-    for day in range(-20, 21):
-        cur_day = birthday_dt + datetime.timedelta(days=day)
-        # x_series.append(cur_day.strftime("%Y-%m-%d"))
-        x_series.append(cur_day)
-        y_series.append(1.2)
+    print(f"Day elapsed: {rst[0]}")
+    phy, emo, men = (f"{v:.1%}" for v in rst[1])
+    print(f"Physical: {phy}, Emotional: {emo}, Mental: {men} as of {targetday}")
 
-    plt.title("Biorhythms")
-    plt.xlabel('Date')
-    plt.ylabel('Value')
-    plt.plot(x_series, y_series)
-    plt.grid(True)
-    plt.xticks(rotation=45)
+    ### plot
+    view_range = 30
+    # elasped from birthday
+    start_point = rst[0] - view_range // 2
+    end_point = rst[0] + view_range // 2 + 1
+    x_value = range(start_point, end_point)
+    x_label = [(birthday_dt + datetime.timedelta(days=v)).strftime("%Y-%m-%d") for v in x_value]
+    print(x_label)
 
-    plt.show()
+    # x_value = range(rst[0] - view_range // 2, rst[0] + view_range // 2 + 1)  # elapsed days
+    # x_label = [(birthday_dt + datetime.timedelta(days=v)).strftime("%Y-%m-%d") for v in x_value]
+    # x_value = []
+    # x_label = []
+
+    # for x in range(- view_range // 2, view_range // 2 + 1):
+    #     x_day = birthday_dt + datetime.timedelta(days=rst[0] + x)
+
+    # for eday in x_value:
+    #     cur_day = birthday_dt + datetime.timedelta(days = eday)
+    #     print(cur_day)
+
+    # elapsed_td = targetday_dt - birthday_dt
+    # x_series = []
+    # y_series = []
+    # for day in range(-20, 21):
+    #     cur_day = birthday_dt + datetime.timedelta(days=day)
+    #     # x_series.append(cur_day.strftime("%Y-%m-%d"))
+    #     x_series.append(cur_day)
+    #     y_series.append(1.2)
+    #
+    # plt.title("Biorhythms")
+    # plt.xlabel('Date')
+    # plt.ylabel('Value')
+    # plt.plot(x_series, y_series)
+    # plt.grid(True)
+    # plt.xticks(rotation=45)
+    #
+    # plt.show()
 
 
 if __name__ == '__main__':
