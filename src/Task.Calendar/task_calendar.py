4fd48d08fa0a94e7d38114cde5787fb1fa687de7
mohenjo <66149782+mohenjo@users.noreply.github.com>
2020-09-25 16:12:12 +0900

    Update task_calendar.py

--- src/Task.Calendar/task_calendar.py
+++ src/Task.Calendar/task_calendar.py
@@ -4,7 +4,14 @@ import calendar
 def main():
     ### calendar 모듈 사용
     cal = calendar.calendar(2020)
-    print(type(cal))
+    print(cal)
+    ### 자체 함수 생성
+    cal = get_calendar(2020)
+    print(cal)
+
+def get_calendar(year:int):
+    rst = ""
+    return rst
 
 
 if __name__ == '__main__':
