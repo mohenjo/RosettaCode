5af3122223ea38955d3ac9bd92664facd655f09b
mohenjo <66149782+mohenjo@users.noreply.github.com>
2020-09-29 13:01:30 +0900

    Add C# solution

---
+++ Biorhythms/ConsolePlayground/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsBiorhythms;
+
+namespace ConsolePlayground
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime birthday = new DateTime(1943, 03, 09);
+            DateTime checkday = new DateTime(1972, 07, 11);
+            Biorhythm bio = new Biorhythm(birthday, checkday);
+
+            Console.WriteLine(bio.DaysElapsed);
+        }
+    }
+}
---
+++ Biorhythms/CsBiorhythms/Biorhythm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsBiorhythms
+{
+    public class Biorhythm
+    {
+
+        public Biorhythm(DateTime birthday, DateTime checkday)
+        {
+            this.Birthday = birthday;
+            this.CheckDay = checkday;
+
+            DaysElapsed = (checkday - birthday).Days;
+            if (DaysElapsed < 15)
+            {
+                throw new ArgumentException("생일과 최소 15일 이상 차이나는 날짜를 체크해야 합니다.");
+            }
+
+
+        }
+
+        public DateTime Birthday { get; private set; }
+
+        public DateTime CheckDay { get; private set; }
+
+        public int DaysElapsed { get; private set; }
+    }
+}
