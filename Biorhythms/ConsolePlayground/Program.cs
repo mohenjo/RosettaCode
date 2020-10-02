a3cc43bd74e0e21bb140fbefa1a135ec018edd55
mohenjo <66149782+mohenjo@users.noreply.github.com>
2020-10-02 12:23:20 +0900

    Update Program.cs
    Console 테스트 완료

--- Biorhythms/ConsolePlayground/Program.cs
+++ Biorhythms/ConsolePlayground/Program.cs
@@ -14,9 +14,12 @@ namespace ConsolePlayground
             DateTime birthday = new DateTime(1943, 03, 09);
             DateTime checkday = new DateTime(1972, 07, 11);
             Biorhythm bio = new Biorhythm(birthday, checkday);
-            
 
-            Console.WriteLine(bio.DaysElapsed);
+            Console.WriteLine($"Days elapsed: {bio.DaysElapsed:#,##0}");
+            Dictionary<Biorhythm.BioType, double> bioResult = bio.GetBiorhytms();
+            Console.WriteLine($"신체: {bioResult[Biorhythm.BioType.Physical]:f1}%");
+            Console.WriteLine($"감성: {bioResult[Biorhythm.BioType.Emotional]:f1}%");
+            Console.WriteLine($"지성: {bioResult[Biorhythm.BioType.Mental]:f1}%");
         }
     }
 }
