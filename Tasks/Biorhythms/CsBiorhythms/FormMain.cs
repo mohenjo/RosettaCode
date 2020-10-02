689b4f9b1231cd76c1677432d8c0f85309f9df76
mohenjo <66149782+mohenjo@users.noreply.github.com>
2020-10-02 16:22:47 +0900

    Update Biorhythms C# implementation

--- Tasks/Biorhythms/CsBiorhythms/FormMain.cs
+++ Tasks/Biorhythms/CsBiorhythms/FormMain.cs
@@ -15,6 +15,38 @@ namespace CsBiorhythms
         public FormMain()
         {
             InitializeComponent();
+            Initialize(); // 윈폼 프로젝트를 초기화/개인화
+        }
+
+
+        public DateTime Birthday { get; private set; }
+
+        public DateTime CheckDay { get; private set; }
+
+        private void Initialize()
+        {
+        }
+
+        private void buttonShowChart_Click(object sender, EventArgs e)
+        {
+            this.Birthday = dateTimePickerBirthday.Value;
+            this.CheckDay = dateTimePickerCheckDay.Value;
+            InitChar();
+        }
+
+        private void InitChar()
+        {
+            chartBiorhythms.ChartAreas.Clear();
+            chartBiorhythms.Series.Clear();
+
+            chartBiorhythms.ChartAreas.Add("Biorhythms");
+            chartBiorhythms.ChartAreas["Biorhythms"].AxisX.Minimum = -15;
+        }
+
+        private void InitDateSet()
+        {
+            DateTime BeginDate = CheckDay.AddDays(-VIEW_DAYS_SPAN / 2);
+            DateTime EndDate = CheckDay.AddDays(VIEW_DAYS_SPAN / 2);
         }
     }
 }
---
+++ Tasks/Biorhythms/CsBiorhythms/Globals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsBiorhythms
+{
+    public static class Globals
+    {
+        public const int VIEW_DAYS_SPAN = 40; // 바이오리듬 차트를 나타낼 범위(일)
+
+    }
+}
