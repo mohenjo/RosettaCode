c5cd3d5e60488c686d67e447b5eba9d3f0592228
mohenjo <66149782+mohenjo@users.noreply.github.com>
2020-10-05 19:19:38 +0900

    Update main.py

--- Tasks/Euler'sSumOfPowersConjecture/main.py
+++ Tasks/Euler'sSumOfPowersConjecture/main.py
@@ -2,10 +2,11 @@ def main():
     rst = bf_cal()
     print(f"{rst[0]}^5 + {rst[1]}^5 + {rst[2]}^5 + {rst[3]}^5 = {rst[4]}^5")
 
+
 def bf_cal():
     max_n = 250
     pwr_pool = [n ** 5 for n in range(max_n)]
-    y_pwr_pool = {n ** 5 : n for n in range(max_n)}
+    y_pwr_pool = {n ** 5: n for n in range(max_n)}
     for x0 in range(1, max_n):
         print(f"processing {x0} in (0..250)")
         for x1 in range(x0, max_n):
@@ -16,7 +17,8 @@ def bf_cal():
                         y = y_pwr_pool[y_pwr5]
                         if y not in (x0, x1, x2, x3):
                             return x0, x1, x2, x3, y
-                       
+
+
 if __name__ == "__main__":
     main()
     # 27^5 + 84^5 + 110^5 + 133^5 = 144^5
