4456377b70ac25042a16ddb50bfaf6f3f1e0a439
mohenjo <66149782+mohenjo@users.noreply.github.com>
2020-10-10 11:15:00 +0900

    Add solution
    Sequence-SmallestNumberWithExactlyNDivisors

---
+++ Tasks/Sequence-SmallestNumberWithExactlyNDivisors/pysolution/pysolution.py
@@ -0,0 +1,25 @@
+def main():
+    rst = generate_series()
+    for _ in range(15):
+        print(next(rst))
+
+def generate_series():
+    cur_idx = 1
+    cur_val = 1
+    while True:
+        if get_num_divisors(cur_val) == cur_idx:
+            cur_idx += 1
+            yield cur_val
+            cur_val = 1
+        cur_val +=1
+
+def get_num_divisors(anum:int):
+    divisors = set()
+    for n in range(1, int(anum ** 0.5) + 1):
+        if anum % n == 0:
+            divisors.add(n)
+            divisors.add(anum // n)
+    return len(divisors)
+
+if __name__ == "__main__":
+    main()
